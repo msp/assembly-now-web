0c11e7d163257e0aa91fb09fdcf47a9338026d48
msp <matt@cenatus.org>
2020-08-26 17:41:54 +0100

    Go fullscreen

--- public/js/gui.js
+++ public/js/gui.js
@@ -7,12 +7,35 @@ function init() {
 }
 
 function addEventListeners() {
-    document.querySelector('#cameraBtn').addEventListener('click', Networking.openUserMedia);
+    document.querySelector('#cameraBtn').addEventListener('click', openCamera);
     document.querySelector('#hangupBtn').addEventListener('click', Networking.hangUp);
     document.querySelector('#createBtn').addEventListener('click', Networking.createRoom);
     document.querySelector('#joinBtn').addEventListener('click', Networking.joinRoom);
     document.querySelector('#playBtn').addEventListener('click', ExperienceTimelines.play);
     document.querySelector('#stopBtn').addEventListener('click', ExperienceTimelines.stop);
+}
+
+function fullscreen() {
+    const elem = document.documentElement;
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) { /* Firefox */
+        elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE/Edge */
+        elem.msRequestFullscreen();
+    }
+}
+
+async function openCamera() {
+    await Networking.openUserMedia();
+    hideControls()
+    fullscreen();
+}
+
+function hideControls() {
+    gsap.set("#buttons", { opacity: 0 });
 }
 
 export { init };

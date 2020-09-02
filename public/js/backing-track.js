b8cf96f1f22f47583b80b00aff9ad56ec14acfe1
msp <matt@cenatus.org>
2020-09-02 14:32:47 +0100

    Drop the backing track level a little

--- public/js/backing-track.js
+++ public/js/backing-track.js
@@ -2,7 +2,7 @@ async function play() {
     const audio = new Audio('https://www.dropbox.com/s/udxc236gh8xbs5t/20200823-assembly-now-draft-v1.0.0.mp3?raw=1');
     audio.type = 'audio/mpeg';
     audio.loop = true;
-    audio.volume = 0.7;
+    audio.volume = 0.6;
 
     try {
         await audio.play();

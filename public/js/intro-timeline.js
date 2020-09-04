16efb6f2823fe598f4850b0ebccedadb10353c7b
msp <matt@cenatus.org>
2020-09-04 12:29:04 +0100

    Allow longer pauses to read the intro copy

--- public/js/intro-timeline.js
+++ public/js/intro-timeline.js
@@ -13,19 +13,19 @@ function play({ onComplete = () => console.log("nada") } = {}) {
         tl.to("#splash.screen #title", { opacity: 0, duration: 1 });
         tl.to("#splash.screen #loading-wrapper", { opacity: 0, duration: 1 });
         tl.to("#splash.screen h3", { opacity: 0, duration: 1 });
-        tl.to("#splash.screen", { opacity: 0, duration: 2 });
+        tl.to("#splash.screen", { opacity: 0, duration: 1 });
         tl.set("#splash.screen", { display: "none" });
 
-        tl.to("#intro.screen", { opacity: 1, display: "grid", duration: 2 });
+        tl.to("#intro.screen", { opacity: 1, display: "grid", duration: 1 });
         tl.to("#intro.screen summary", { opacity: 1, duration: 1 });
         tl.to("#intro.screen summary #intro-one", { opacity: 1, duration: 1 });
-        tl.to({}, 3, {}); //pause
+        tl.to({}, 5, {}); //pause
         tl.to("#intro.screen summary #intro-two", { opacity: 1, duration: 1 });
-        tl.to({}, 3, {}); //pause
+        tl.to({}, 5, {}); //pause
         tl.to("#intro.screen summary #housekeeping", { opacity: 1, duration: 1 });
-        tl.to({}, 3, {}); //pause
+        tl.to({}, 4, {}); //pause
         tl.to("#intro.screen footer", { opacity: 1, duration: 1 });
-        tl.to({}, 3, {}); //pause
+        tl.to({}, 5, {}); //pause
         tl.to("#intro.screen nav", { opacity: 1, duration: 0.5, yoyo: true, repeat: 4 });
         tl.addPause("#intro.screen", requestCameraAccess, [tl]);
         tl.to("#intro.screen", { opacity: 0, duration: 2 });

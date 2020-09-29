6ac78c453897431dfcee25b675d3d1078bf6c274
msp <matt@cenatus.org>
2020-09-29 13:00:30 +0100

    Try again for analytics?
    Do a manual Firebase initialisation rather than through magic hosting URLs

---
+++ public/js/firebase.js
@@ -0,0 +1,19 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCQE0kMpFdV89MMG-tHwKnW6TojF9W9OFA",
+    authDomain: "assembly-now-rtc.firebaseapp.com",
+    databaseURL: "https://assembly-now-rtc.firebaseio.com",
+    projectId: "assembly-now-rtc",
+    storageBucket: "assembly-now-rtc.appspot.com",
+    messagingSenderId: "832458070870",
+    appId: "1:832458070870:web:4263727cccc3a4df4d54f7",
+    measurementId: "G-N9CPJJ57RM"
+};
+
+function init() {
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+}
+
+export { init };
--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,11 @@
 import * as ExperienceTimelines from './experience-timelines.js';
+import * as FBase from './firebase.js';
 import * as GUI from './gui.js';
 import * as Intro from './intro-timeline.js';
 
 
 function main() {
+    FBase.init()
     GUI.init();
 
     Intro.play({

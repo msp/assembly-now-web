f032dc1b5ce6dcc6a0b05360f32d6e451f10762b
msp <matt@cenatus.org>
2020-09-29 13:24:50 +0100

    Give up on Firebase Analytics and go direct to GA

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,9 @@
 import * as ExperienceTimelines from './experience-timelines.js';
-import * as FBase from './firebase.js';
 import * as GUI from './gui.js';
 import * as Intro from './intro-timeline.js';
 
 
 function main() {
-    FBase.init()
     GUI.init();
 
     Intro.play({
